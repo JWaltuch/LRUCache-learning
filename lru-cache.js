4585459e116a6e700e9ca90e76c0c84c2ecb56ff
JWaltuch <jenna.waltuch@gmail.com>
2020-05-07 19:45:54 -0400

    building DLL

--- lru-cache.js
+++ lru-cache.js
@@ -1,15 +1,49 @@
 //Type your LRU Cache below!
 
-class LRUCache{
-    constructor(maxSize) {
-         this.maxSize = maxSize || 1
-    }
-    insertKeyValuePair() {
+//add to the front, remove the last if cache is full
+//when accessing item, move to front of cache
 
-    }
+class LRUCache {
+  constructor(maxSize) {
+    this.maxSize = maxSize || 1;
+    this.cache = {};
+    this.length = 0;
+    this.list = new DoublyLinkedList();
+  }
+  insertKeyValuePair() {}
 
-    getMostRecentKey(){
-        
-    }
+  getMostRecentKey() {}
+
+  getValueFromKey() {}
 }
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  insertNewNode(key, value) {
+    let node = new Node(key, value);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.head.prev = node;
+      node.next = this.head;
+      this.head = node;
+    }
+    return node;
+  }
+  evict() {}
+}
+
+class Node {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
 module.exports = LRUCache;
