82892e42e429558ad29b1f5a8989e0670a0be469
JWaltuch <jenna.waltuch@gmail.com>
2020-05-07 20:06:02 -0400

    cache completed

--- lru-cache.js
+++ lru-cache.js
@@ -10,11 +10,35 @@ class LRUCache {
     this.length = 0;
     this.list = new DoublyLinkedList();
   }
-  insertKeyValuePair() {}
+  insertKeyValuePair(key, value) {
+    if (!this.cache[key]) {
+      let node = this.list.insertNewNode(key, value);
+      this.cache[key] = node;
+      this.length++;
+    } else {
+      this.list.moveToHead(this.cache[key], key, value);
+    }
+    if (this.length > this.maxSize) {
+      let nodeToDelete = this.list.evict();
+      let keyToGetRidOf = nodeToDelete.key;
+      delete this.cache[keyToGetRidOf];
+      this.length--;
+    }
+  }
 
-  getMostRecentKey() {}
+  getMostRecentKey() {
+    return this.list.head ? this.list.head : null;
+  }
 
-  getValueFromKey() {}
+  getValueFromKey(key) {
+    if (!this.cache[key]) {
+      return null;
+    } else {
+      let nodeToMove = this.cache[key];
+      this.list.moveToHead(nodeToMove);
+      return nodeToMove.value;
+    }
+  }
 }
 
 class DoublyLinkedList {
@@ -34,7 +58,44 @@ class DoublyLinkedList {
     }
     return node;
   }
-  evict() {}
+  evict() {
+    let nodeToDelete = {};
+    if (this.head === this.tail) {
+      nodeToDelete = this.head;
+      this.head = null;
+      this.tail = null;
+    } else {
+      nodeToDelete = this.tail;
+      const prevNode = this.tail.prev;
+      prevNode.next = null;
+      this.tail = prevNode;
+    }
+    return nodeToDelete;
+  }
+  moveToHead(node, newKey, newVal) {
+    if (newKey && newVal) {
+      node.key = newKey;
+      node.value = newVal;
+    }
+    if (node === this.tail && node !== this.head) {
+      let prevNode = this.tail.prev;
+      prevNode.next = null;
+      this.tail = prevNode;
+
+      node.prev = null;
+      this.head.prev = node;
+      node.next = this.head;
+      this.head = node;
+    } else if (node !== this.head) {
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
+
+      node.prev = null;
+      this.head.prev = node;
+      node.next = this.head;
+      this.head = node;
+    }
+  }
 }
 
 class Node {
